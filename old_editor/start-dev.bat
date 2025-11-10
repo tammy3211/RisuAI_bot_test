f7c250d35c790b97f2c682927f58560f9cced337
tammy3211 <miningtom01@gmail.com>
2025-11-11 02:19:26 +0900

    chore: old_editor 폴더를 Git 추적에서 제외

--- old_editor/start-dev.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo Installing editor dependencies...
-call pnpm install
-
-echo.
-echo Starting editor development server...
-call pnpm dev
--- old_editor/start-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Installing editor dependencies..."
-pnpm install
-
-echo ""
-echo "Starting editor development server..."
-pnpm dev
